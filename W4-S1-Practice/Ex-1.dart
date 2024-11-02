7f2c79b911055175b325aff082a3ebc99a2fdfc3
hengvisal0405 <imheangsocheata1900@gmail.com>
2024-11-03 03:29:26 +0700

    Flutter

---
+++ W4-S1-Practice/Ex-1.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+const String icon1 = "/Users/notvalsy/Documents/CADT-Y3T1/mobile_development/flutter_app/assets/icons/icons8-travelling-64.png";
+const String icon2 = "/Users/notvalsy/Documents/CADT-Y3T1/mobile_development/flutter_app/assets/icons/icons8-roller-skating-50.png";
+class HobbiesCard extends StatelessWidget {
+  const HobbiesCard({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: const Text("My hobbies "),
+          centerTitle: false,
+        ),
+        body: Center(
+          // margin: const EdgeInsets.all(40),
+          child: Container(
+            margin:const EdgeInsets.only(top:40),
+                      child: Column(
+              children: [
+                Container(
+                  child: Card(
+                    color: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      width: 500,
+                      height: 100,
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding:const EdgeInsets.only(right: 20,left: 30),
+                              child: Image.asset(
+                              icon1, 
+                              width: 30,
+                              height: 30, 
+                            ),
+                            ),
+                            const Text(
+                              'Travelling',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top:10),
+                  child: Card(
+                    color: const Color(0xFF5D6D7E),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      width: 500,
+                      height: 100,
+                      padding: const EdgeInsets.all(20),
+                      child:  Container(
+                        child:Row(
+                          children: [
+                            Padding(
+                              padding:const EdgeInsets.only(right: 20, left: 30),
+                            child: Image.asset(
+                                icon2,
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+
+                            const Text(
+                              'Skating',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          
+
+        
+        
+        ),
+      ),
+    );
+  }
+  
+}
+
+void main() {
+  runApp(const HobbiesCard());
+}
---
+++ W4-S1-Practice/Ex-3.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+enum Buttons {
+  btnSubmit(
+    backgroundColor:Colors.blue,
+    icon:'/Users/notvalsy/Documents/CADT-Y3T1/mobile_development/flutter_app/assets/icons/icons8-tick-50.png',
+    text: 'Submit',
+  ),
+  btnTime(
+    backgroundColor: Colors.green,
+    icon:'/Users/notvalsy/Documents/CADT-Y3T1/mobile_development/flutter_app/assets/icons/icons8-time-24.png',
+    text: 'Time',
+  ),
+  btnAccount(
+    backgroundColor: Color(0xFFB3B6B7),
+    icon:'/Users/notvalsy/Documents/CADT-Y3T1/mobile_development/flutter_app/assets/icons/icons8-account-50.png',
+    text: 'Account',
+  );
+
+  final String icon;
+  final Color backgroundColor;
+  final String text;
+
+  const Buttons({
+    required this.icon,
+    required this.backgroundColor,
+    required this.text,
+  });
+}
+
+void main() {
+  void onPressed() {
+    print("Jelllo");
+  }
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Customer buttons"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(top: 0),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                    // primary:Colors.blue,
+                    backgroundColor: Buttons.btnSubmit.backgroundColor,
+                    minimumSize: const Size(700, 60)),
+                label: Text(
+                  Buttons.btnSubmit.text,
+                  style: const TextStyle(
+                    color: Color(0xFF7F8C8D),
+                    fontSize: 25,
+                  ),
+                ),
+                icon: Image.asset(
+                  Buttons.btnSubmit.icon,
+                  width: 25,
+                  height: 25,
+                ),
+                onPressed: onPressed,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              child: ElevatedButton.icon(
+                onPressed: onPressed,
+                style: ElevatedButton.styleFrom(
+                    // primary:Colors.blue,
+                    backgroundColor: Buttons.btnTime.backgroundColor,
+                    minimumSize: const Size(700, 60)),
+                label: Text(
+                  Buttons.btnTime.text,
+                  style: const TextStyle(
+                    color: Color(0xFF7F8C8D),
+                    fontSize: 25,
+                  ),
+                ),
+                icon: Image.asset(
+                  Buttons.btnTime.icon,
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              child: ElevatedButton.icon(
+                label:Text(
+                  Buttons.btnAccount.text,
+                  style: const TextStyle(
+                    color: Color(0xFF7F8C8D),
+                    fontSize: 25,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    // primary:Colors.blue,
+                    backgroundColor: Buttons.btnAccount.backgroundColor,
+                    minimumSize: const Size(700, 60)),
+                icon: Image.asset(
+                  Buttons.btnAccount.icon,
+                  width: 30,
+                  height: 30,
+                ),
+                onPressed: onPressed,
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  ));
+}
