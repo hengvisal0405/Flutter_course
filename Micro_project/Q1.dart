b4082b0a4c8d0db5ac8fb8cc8b3a3fedeab6ee7d
hengvisal0405 <imheangsocheata1900@gmail.com>
2024-11-13 17:39:11 +0700

    Code upadte

--- Micro_project/Q1.dart
+++ Micro_project/Q1.dart
@@ -13,16 +13,28 @@ class Quiz {
   void addParticipant(Participant participant) {
     participants.add(participant);
   }
-
   void calculateResults() {
     for (var participant in participants) {
-      participant.calculateScore(this); // Calls calculateScore for each participant
+      int score = 0;
+      for (var question in questions) {
+        if (question is SingleChoice) {
+          if (participant.singleAnswer == question.getCorrectAnswer()?.first) {
+            score += 1;
+          }
+        } else if (question is MultipleChoice) {
+          if (question.getCorrectAnswer()!.every((answer) => participant.multipleAnswers.contains(answer)) && participant.multipleAnswers.length == question.getCorrectAnswer()!.length) {
+            score += 1;
+          }
+        }
+      }
+      participant.score = score; 
     }
   }
 
   void displayResults() {
     for (var participant in participants) {
-      print('${participant.firstName} ${participant.lastName} scored: ${participant.score}');
+      print(
+          '${participant.firstName} ${participant.lastName} scored: ${participant.score}');
     }
   }
 }
@@ -43,67 +55,41 @@ class Participant {
   String? firstName;
   String? lastName;
   int score = 0;
+  String? singleAnswer; 
+  List<String> multipleAnswers = []; 
+
   Participant(this.firstName, this.lastName);
-
-  void calculateScore(Quiz quiz) {
-    for (var question in quiz.questions) {
-      if (question is SingleChoice) {
-        // Compare with the first correct answer in the list
-        if (question.SingleAnswerChoose == question.getCorrectAnswer()?.first) {
-          score += 1;
-        }
-      } else if (question is MultipleChoice) {
-        int countCorrect;
-        countCorrect = question.correctAnswer!.where((ans) => question.MultipleAnswerChoose.contains(ans)).length;
-        score += countCorrect;
-      }
-    }
+  void selectSingleAnswer(String answer) {
+    singleAnswer = answer;
+  }
+  void selectMultipleAnswers(List<String> answers) {
+    multipleAnswers = answers;
   }
 }
-
 class SingleChoice extends Question {
-  String? SingleAnswerChoose;
   SingleChoice(super.title, super.correctAnswer, super.options);
-
-  void selectAnswerSingle(String answer) {
-    if (options.contains(answer)) {
-      SingleAnswerChoose = answer;
-    }
-  }
 }
-
 class MultipleChoice extends Question {
-  List<String> MultipleAnswerChoose = [];
-
   MultipleChoice(super.title, super.correctAnswer, super.options);
-
-  void selectAnswerMultiple(List<String> answers) {
-    for (var answer in answers) {
-      if (options.contains(answer)) {
-        MultipleAnswerChoose.add(answer);
-      }
-    }
-  }
 }
-
 void main() {
   Quiz quiz = Quiz();
-
   SingleChoice q1 = SingleChoice("What is Cat?", ["Water"], ["Fruit", "Water", "Animal"]);
-
-  MultipleChoice q2 = MultipleChoice("Which are the programming languages?", ["Java", "Blue language", "C++", "Dart", "HTML"], ["HTML", "Java", "C++", "Dart"]);
+  MultipleChoice q2 = MultipleChoice("Which are the programming languages?",["HTML", "Java", "C++", "Dart"],["Java", "Blue language", "C++", "Dart", "HTML"]);
 
   quiz.addSingleQuestion(q1);
   quiz.addMultipleQuestions([q2]);
 
   Participant p1 = Participant("Heng", "Visal");
+  p1.selectSingleAnswer("Water");
+  p1.selectMultipleAnswers(["HTML", "Java", "C++", "Dart"]);
+
   Participant p2 = Participant("Heng", "Panharanith");
+  p2.selectSingleAnswer("Water");
+  p2.selectMultipleAnswers(["Java", "Dart"]);
 
   quiz.addParticipant(p1);
   quiz.addParticipant(p2);
-
-  q1.selectAnswerSingle("Water");
-  q2.selectAnswerMultiple(["HTML", "Java", "C++", "Dart"]);
 
   quiz.calculateResults();
   quiz.displayResults();
